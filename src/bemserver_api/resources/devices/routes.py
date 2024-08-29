3a946bb25ec19a2c00cc020d4b270692acb933b6
githaefrancis <mureithigithae@gmail.com>
2024-08-29 11:38:53 +0300

    mod(Aggregate) - return consumption total and cost with aggregate endpoint

--- src/bemserver_api/resources/devices/routes.py
+++ src/bemserver_api/resources/devices/routes.py
@@ -2,10 +2,11 @@ from flask.views import MethodView
 
 from flask_smorest import abort
 
-from bemserver_core.model import Device, DeviceCategory,DeviceByTimeseries
+from bemserver_core.model import Device, DeviceCategory,DeviceByTimeseries, Campaign, Building, Site,UserGroupByCampaign,UserGroup, User, UserByUserGroup
 from bemserver_core.model.device import DeviceStatus
 from bemserver_api import Blueprint
 from bemserver_api.database import db
+from bemserver_core.authorization import get_current_user
 
 from .schema import DeviceSchema, DeviceGetQueryArgsSchema, DeviceCategorySchema, DeviceByTimeSeriesSchema, DeviceResponseSchema
 
@@ -17,6 +18,22 @@ blp = Blueprint(
 )
 
 
+def get_user_organization():
+    query = (
+        db.session.query(Campaign)
+        .join(UserGroupByCampaign, UserGroupByCampaign.campaign_id == Campaign.id)
+        .join(UserGroup, UserGroupByCampaign.user_group_id == UserGroup.id)
+        .join(UserByUserGroup, UserGroup.id == UserByUserGroup.user_group_id)
+        .join(User, UserByUserGroup.user_id == User.id)
+        .filter(User.id == get_current_user().id)
+    )
+
+    # Execute the query and fetch results
+    campaign_id = query.first().id
+
+    return campaign_id
+
+
 @blp.route("/")
 class DevicesViews(MethodView):
     @blp.login_required
@@ -24,7 +41,16 @@ class DevicesViews(MethodView):
     @blp.response(200, DeviceResponseSchema(many=True))
     def get(self):
         """List devices"""
-        return Device.get()
+        # return Device.get()
+        campaign_id = get_user_organization()
+        query = (
+            db.session.query(Device)
+            .join(Building)
+            .join(Site)
+            .join(Campaign)
+            .filter(Campaign.id == campaign_id)
+        )
+        return query.all()
 
     @blp.login_required
     @blp.etag
